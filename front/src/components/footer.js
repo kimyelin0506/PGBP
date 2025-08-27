1681c0d4ea13b1d965eb766e3bb125a59d42f067
kimyelin <annalin1004@gmail.com>
2025-08-27 11:51:01 +0900

    feat: footer

---
+++ front/src/components/footer.js
@@ -0,0 +1,3 @@
+export function renderFooter() {
+    return `<div class="footer">COPYRIGHT ⓒ 2021 KOREA ENERGY AGENCY ALL RIGHTS RESERVED 44538 울산광역시 중구 종가로 323 (우정동 528-1) 대표전화 : 052-920-0114</div>			`;
+}
---
+++ front/src/layout.js
@@ -0,0 +1,11 @@
+import { Header } from '@/components/header';
+
+export default function Layout() {
+    return `
+    <>
+        <div>
+        <${Header} />
+        </div>
+    </>
+    `; 
+}
